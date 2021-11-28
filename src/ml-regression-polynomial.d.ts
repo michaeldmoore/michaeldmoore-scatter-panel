7b747f913300e7115b1abfe1673be9c455dcef62
Michael Moore <mdmoore@gmail.com>
2021-11-28 22:18:33 +0100

    Simplify regression d.ts files

--- src/ml-regression-polynomial.d.ts
+++ src/ml-regression-polynomial.d.ts
@@ -1,17 +1,7 @@
-import BaseRegression from 'ml-regression-base';
-
 declare namespace PolynomialRegression {
   export interface PolynomialRegressionModel {
     name: 'PolynomialRegression';
   }
-}
-
-declare class PolynomialRegression extends BaseRegression {
-  constructor(x: number[], y: number[], order: number);
-
-  static load(model: PolynomialRegression.PolynomialRegressionModel): PolynomialRegression;
-
-  toJSON(): PolynomialRegression.PolynomialRegressionModel;
 }
 
 export = PolynomialRegression;
--- src/ml-regression-power.d.ts
+++ src/ml-regression-power.d.ts
@@ -1,17 +1,7 @@
-import BaseRegression from 'ml-regression-base';
-
 declare namespace PowerRegression {
   export interface PowerRegressionModel {
     name: 'PowerRegression';
   }
-}
-
-declare class PowerRegression extends BaseRegression {
-  constructor(x: number[], y: number[]);
-
-  static load(model: PowerRegression.PowerRegressionModel): PowerRegression;
-
-  toJSON(): PowerRegression.PowerRegressionModel;
 }
 
 export = PowerRegression;
